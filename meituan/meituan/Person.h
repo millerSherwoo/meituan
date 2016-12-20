c3a9d8ab667ec5be6cde1e5a3f70106bd37afdbe
Miller <473916200@qq.com>
2016-12-20 17:25:58 +0800

    Person类

---
+++ meituan/meituan/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  meituan
+//
+//  Created by Miller_Sherwoo on 2016/12/20.
+//  Copyright © 2016年 sostvcn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ meituan/meituan/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  meituan
+//
+//  Created by Miller_Sherwoo on 2016/12/20.
+//  Copyright © 2016年 sostvcn. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
